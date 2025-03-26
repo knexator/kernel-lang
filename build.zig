850dae0339faa0592f71720d915a05cc769e83dd
knexator <daniel.hurtadoramirez314@gmail.com>
2025-03-26 18:19:01 +0100

    build: check step

--- build.zig
+++ build.zig
@@ -29,4 +29,11 @@ pub fn build(b: *std.Build) void {
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&run_exe_unit_tests.step);
+
+    const exe_check = b.addExecutable(.{
+        .name = "vaulang",
+        .root_module = exe_mod,
+    });
+    const check = b.step("check", "Check if it compiles");
+    check.dependOn(&exe_check.step);
 }
